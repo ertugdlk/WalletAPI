e209460caadef97a9512cb3dcddcb30dce6105ac
Tugkan <tugkanpilka@gmail.com>
2021-04-01 16:23:03 +0300

    CQ improvements

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
-const Cors = require("cors");
+const cors = require("cors");
 const _ = require("lodash");
 
 const express = require("express");
 const BodyParser = require("body-parser");
 const mongoClient = require('./database/config');
 
-const App = express();
+const { MONGODB_CONNECT_ERROR } = require('./source/variables/errors');
 
-App.use(BodyParser.json());
-App.use(BodyParser.urlencoded({ extended: true }));
 
-mongoClient.connect()
-    .then(() => {
-      const port = process.env.PORT || 5000;
-      const portStatus = 'Listening on port ' + port;
+const app = express();
 
-      App.listen(port);
-      console.log(portStatus);  
+app.use(cors());
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
 
-      App.get("/", (req, res) => res.json({ msg: "tugkan API service"}));
+async function startMongoDb() {
+  try {
+    await mongoClient.connect();
+  } catch (error) {
+    console.log(MONGODB_CONNECT_ERROR, error);
+  }
+}
 
-      //Routes
-      App.use("/wallet", require("./routes/WalletRoute"));
-});
+async function startExpress() {
+  const port = process.env.PORT || 5000;
+  const portStatus = 'Listening on port ' + port;
+
+  app.listen(port);
+  console.log(portStatus);  
+
+  app.get("/", (req, res) => res.json({ msg: "tugkan API service"}));
+
+  //Routes
+  app.use("/wallet", require("./routes/WalletRoute"));
+}
+
+startMongoDb();
+startExpress();
---
+++ source/variables/errors.js
@@ -0,0 +1,5 @@
+const MONGODB_CONNECT_ERROR = 'There was an error during connect process of mongoDb';
+
+module.exports = {
+    MONGODB_CONNECT_ERROR
+};
