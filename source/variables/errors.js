d62605f5dd904185297b434e3c2ea6532023138e
Tugkan <tugkanpilka@gmail.com>
2021-04-03 10:33:26 +0300

    Source folder recreated

---
+++ source/variables/errors.js
@@ -0,0 +1,7 @@
+const MONGODB_CONNECT_ERROR = 'There was an error during connect process of mongoDb';
+const EXTERNAL_REQUEST_FAIL = 'Ups, an error occured. Please try again later.'
+
+module.exports = {
+    MONGODB_CONNECT_ERROR,
+    EXTERNAL_REQUEST_FAIL
+};
