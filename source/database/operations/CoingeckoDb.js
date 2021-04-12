5954d1a66c6dca42a392d08fe160b95a0b0bc555
Tuğkan Pilka <tugkanpilka@gmail.com>
2021-04-12 11:16:14 +0300

    Mongo failure

--- source/database/operations/CoingeckoDb.js
+++ source/database/operations/CoingeckoDb.js
@@ -4,22 +4,26 @@ const { mongoClient } = require('../config');
 const coingeckoApi = require('../../APIs/coingeckoAPI');
 
 async function saveMarketData() {
+    console.log('Starting to fetch market data from Coingecko API');
+
     const database = mongoClient.db('test');
     const oldData = database.collection('coingeckoMarket');
 
     try {
+        console.log(`Start: ${new Date()}`);
+
         const newData = await coingeckoApi.fetchCoinMarket();
         const flattenData = _.flatten(newData);
 
-        flattenData.forEach((coin, index) => {
-            console.log(index);
+        for (const coin of flattenData) {
             oldData.findOneAndUpdate(
                 { id: coin.id },
                 { $set: coin },
                 { upsert: true },
             );
-        });
+        }
 
+        console.log(`End: ${new Date()}`);
         console.log(`Length of Coingecko API response: ${flattenData.length}`);
         console.log('MongoDB updated by Coingecko.');
     } catch (error) {
